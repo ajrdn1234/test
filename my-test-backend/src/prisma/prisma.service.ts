5d275c07cff70074b8dd02db127df1789be6574d
ajrdn1234 <ajrdn1234@gmail.com>
2026-09-13 14:30:35 +0000

    NestJS 백엔드 prisma 설정

--- my-test-backend/src/prisma/prisma.service.ts
+++ my-test-backend/src/prisma/prisma.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import 'dotenv/config'
+import { Injectable, OnModuleInit } from '@nestjs/common'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaClient } from '../generated/prisma/client.js'
+
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL!,
+})
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({ adapter })
+    }
+
+    async onModuleInit() {
+        await this.$connect()
+    }
+}
