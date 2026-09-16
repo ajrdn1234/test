80e42dbc1ab2f767dfb2634534820a85ca1e9de7
ajrdn1234 <ajrdn1234@gmail.com>
2026-09-16 07:01:15 +0000

    비트코인 API 변경

--- my-test/app/api/bitcoin/route.ts
+++ my-test/app/api/bitcoin/route.ts
@@ -2,12 +2,6 @@ export const GET = async () => {
     const res = await fetch('https://api.coinone.co.kr/public/v2/ticker_new/KRW/BTC')
     const data = await res.json()
     return Response.json({
-        price: data.tickers[0].last
+        price: parseInt(data.tickers[0].last)
     })
-
-    // const res = await fetch('https://api.upbit.com/v1/ticker?markets=KRW-BTC')
-    // const data = await res.json()
-    // return Response.json({
-    //     price: data[0].trade_price
-    // })
 }
