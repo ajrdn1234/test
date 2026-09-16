63e9e69f09541991854a41798484098a90bfd82d
ajrdn1234 <ajrdn1234@gmail.com>
2026-09-16 00:09:43 +0000

    매매 로직 수정

--- my-test/app/page.tsx
+++ my-test/app/page.tsx
@@ -9,26 +9,27 @@ type Log = {
 
 export default function Home() {
   const [logs, setLogs] = useState<Log[]>([])
-  const [isHolding, setIsHolding] = useState<boolean>(false)
+  const isHoldingRef = useRef<boolean>(false)
   const [price, setPrice] = useState<number>(0)
   const timerRef = useRef<NodeJS.Timeout | null>(null)
+  const total = logs.reduce((acc, cur, index, arr) => index % 2 === 1 ? acc + (cur.price - arr[index - 1].price) : acc + 0, 0)
 
   useEffect(() => {
     timerRef.current = setInterval(async () => {
       const res = await fetch('/api/bitcoin')
       const data = await res.json()
-      if(!isHolding && price > data.price) {
+      if(!isHoldingRef.current && price > data.price) {
         setLogs((preLogs) => [...preLogs, {
           state: 'buy',
           price: data.price
         }])
-        setIsHolding(true)
-      } else if(data.price !== price && isHolding) {
+        isHoldingRef.current = true
+      } else if(isHoldingRef.current && data.price !== price) {
         setLogs((preLogs) => [...preLogs, {
           state: 'sell',
           price: data.price
         }])
-        setIsHolding(false)
+        isHoldingRef.current = false
       }
       setPrice(data.price)
     }, 1000)
@@ -36,11 +37,12 @@ export default function Home() {
     return () => {
       if(timerRef.current) clearInterval(timerRef.current)
     }
-  }, [isHolding, price])
+  }, [price])
 
   return (
     <div>
       <p>현재 가격: {price}원</p>
+      <p>합계: {total}원</p>
       {logs.map((log, index) => <p key={index}>{log.state}: {log.price}</p>)}
     </div>
   )
